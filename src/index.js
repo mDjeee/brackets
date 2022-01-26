3bb27491a575d58c28011379b9c5a3fa09636f98
mDjeee <isejtniyazov@mail.ru>
2022-01-27 03:10:46 +0500

    feat: solve 11 of 20 problems

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(braces, bracketsConfig) {
+  let brackets = '';
+  for(let i=0; i<bracketsConfig.length; i++){
+    brackets += bracketsConfig[i][0] + bracketsConfig[i][1];
+  }
+  let stack = [];
+  for(let bracket in braces){
+    let bracketIndex = brackets.indexOf(bracket);
+
+    if(bracketIndex === -1){
+      continue;
+    }
+
+    if(bracketIndex % 2 === 0){
+      stack.push(bracketIndex + 1);
+    }
+    else {
+      if(stack.pop() !== bracketIndex){
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
