a19c43657185fda5477c182de6fe408161a01adc
mDjeee <isejtniyazov@mail.ru>
2022-01-28 20:51:14 +0500

    feat: solve all problems

--- src/index.js
+++ src/index.js
@@ -1,24 +1,40 @@
 module.exports = function check(braces, bracketsConfig) {
-  let brackets = '';
+
+  let openBreckets = [];
+  let brackets = {};
+  let closed = [];
   for(let i=0; i<bracketsConfig.length; i++){
-    brackets += bracketsConfig[i][0] + bracketsConfig[i][1];
+    openBreckets.push(bracketsConfig[i][0]);
+    brackets[bracketsConfig[i][1]] = bracketsConfig[i][0];
+    closed.push(bracketsConfig[i][1]);
   }
+
   let stack = [];
-  for(let bracket in braces){
-    let bracketIndex = brackets.indexOf(bracket);
 
-    if(bracketIndex === -1){
-      continue;
-    }
+  for(let i=0; i< braces.length; i++){
+    let currentBracket = braces[i];
 
-    if(bracketIndex % 2 === 0){
-      stack.push(bracketIndex + 1);
+    if(stack[stack.length - 1] === currentBracket && closed.includes(currentBracket)){
+      stack.pop();
     }
     else {
-      if(stack.pop() !== bracketIndex){
-        return false;
+      if(openBreckets.includes(currentBracket)){
+        stack.push(currentBracket);
+      }
+      else {
+        if (stack.length === 0){
+          return false;
+        }
+  
+        if(brackets[currentBracket] === stack[stack.length - 1]){
+          stack.pop();
+        }
+        else {
+          return false;
+        }
       }
     }
+    
   }
   return stack.length === 0;
 }
